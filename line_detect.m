327769d80f36e96d62d9d2ee0be674735873d630
Filip von Reis Marlevi <filipvonreismarlevi@n179-p196.eduroam.kth.se>
2019-12-07 12:34:20 +0100

    now lines with the same theta are not taken into acount more then one time

--- line_detect.m
+++ line_detect.m
@@ -38,10 +38,10 @@ P  = houghpeaks(H,10,'threshold',ceil(threshold*max(H(:))));
 
 
 lines = houghlines(img,T,R,P);%,'FillGap',60,'MinLength',20);%'FillGap',60,'MinLength',250);
+thetas = [];
 req_lines = [];
 for i = 1:size(lines,2)
     theta = pi - deg2rad(lines(i).theta + 90);
-    
     rho = lines(i).rho;
     p1 = lines(i).point1;
     x1 = p1(1);
@@ -49,11 +49,15 @@ for i = 1:size(lines,2)
     p2 = lines(i).point2;
     x2 = p2(1);
     y2 = p2(2);
-    angle = -(atan2(y2-y1, x2-x1) * 180 / pi);
-    if angle > lower_angle_lim && angle < upper_angle_lim
-        %approved = angle
-        req_lines = [req_lines; [x1,y1,x2,y2,rho,theta]];
+    if ~ismember(theta, thetas);
+        thetas = [thetas, theta];
+        angle = -(atan2(y2-y1, x2-x1) * 180 / pi);
+        if angle > lower_angle_lim && angle < upper_angle_lim
+            %approved = angle
+            req_lines = [req_lines; [x1,y1,x2,y2,rho,theta]];
+        end
     end
+    
 end
 
 
